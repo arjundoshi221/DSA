842bb3dec791025aeb8df7827fe9a441ecf3e347
Arjun Doshi <73307779+arjundoshi221@users.noreply.github.com>
2025-01-11 21:25:54 -0600

    Time: 29 ms (93.79%), Space: 18 MB (7.89%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,6 +1,8 @@
 class Solution:
     def reverse(self, x: int) -> int:
 
+        MIN, MAX = -2**31, 2**31 - 1
+
         sign = -1 if x < 0 else 1
         
         reversed_x = 0
@@ -11,6 +13,9 @@ class Solution:
             x //= 10
             reversed_x = reversed_x * 10 + digit
 
+        if reversed_x < MIN or reversed_x > MAX:
+            return 0
+        
         return int(reversed_x*sign)
 
     
