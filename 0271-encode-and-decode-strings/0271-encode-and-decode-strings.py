a6d5708f1fdfabab479236f954b7bbffdee94ca3
Arjun Doshi <73307779+arjundoshi221@users.noreply.github.com>
2025-01-14 23:35:44 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -0,0 +1,28 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        encoded_string = ""
+
+        for word in strs:
+            word+=" "
+            encoded_string += word
+        
+        return encoded_string
+        
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        encoder = " "
+
+        decoded_string = s.split(encoder)
+        decoded_string.pop(-1)
+        print(decoded_string)
+
+        return decoded_string
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
