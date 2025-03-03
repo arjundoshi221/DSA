f4a5afb7d24f3c59e6a47dc77981d63148c8467b
Arjun Doshi <73307779+arjundoshi221@users.noreply.github.com>
2025-03-02 19:47:57 -0600

    Time: 0 ms (100%), Space: 12.9 MB (89.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,19 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        target_list = list()
-        len_num = len(nums)
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
 
-        for i in range(len_num):
-            for j in range(i+1, len_num):
-                temp_sum = nums[i] + nums[j]
-                if temp_sum == target:
-                    target_list.append(i)
-                    target_list.append(j)
-                    target_list.sort()
+        indices = {}
 
-        return target_list
+        for i, n in enumerate(nums):
+            indices[n] = i
 
+        for i, n in enumerate(nums):
+            diff = target - n
 
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
         
